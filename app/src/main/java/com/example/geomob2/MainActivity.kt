c0e39ec76dacb5a54ebbdf0812936c2556858cb2
unknown <you@example.com>
2020-06-30 22:27:46 +0100

    Version 2

--- app/src/main/java/com/example/geomob2/MainActivity.kt
+++ app/src/main/java/com/example/geomob2/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recyclerview)
         recyclerView!!.setLayoutManager(LinearLayoutManager(this))
 
-        /*saveTaskPerso("Australia","Kylie Minogue")
+        saveTaskPerso("Australia","Kylie Minogue")
         saveTaskPerso("Australia","Hugh Jackman")
         saveTaskPerso("Australia","Cate Blanchett")
         saveTaskPerso("Brazil","Ayrton Senna")
@@ -135,7 +135,7 @@ class MainActivity : AppCompatActivity() {
         saveTask("Italy",R.drawable.flag_italy,resources.getStringArray(R.array.population_array)[4],
             resources.getStringArray(R.array.description_array)[4],
             resources.getStringArray(R.array.surface_array)[4],
-            R.raw.italy)*/
+            R.raw.italy)
 
 
         getCountry()
